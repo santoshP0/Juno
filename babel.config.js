866abcd7ddd1dd685d56a5bcddeee02b25e56b6f
santoshP0 <santoshpk.mdev@gmail.com>
2026-05-16 21:45:45 +0530

    feat: enhance ProGuard rules and enable R8 obfuscation for release builds; add babel-plugin-transform-remove-console for production

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
+  const isProd = process.env.NODE_ENV === 'production';
   return {
     presets: [
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
     ],
     plugins: [
       'react-native-reanimated/plugin',
+      ...(isProd ? [['transform-remove-console', { exclude: ['error', 'warn'] }]] : []),
     ],
   };
 };
